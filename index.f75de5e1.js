856f856fd55590c8b682c4c16433172bd4315336
Vitalii Nezdoiminoha <vitalii.nezdojminoga@gmail.com>
2024-06-02 23:18:40 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ulElem = document.createElement("ul");
+    for(const key in data)if (data.hasOwnProperty(key)) {
+        const liElem = document.createElement("li");
+        liElem.textContent = key;
+        ulElem.appendChild(liElem);
+        if (typeof data[key] === "object" && data[key] !== null && !!Object.keys(data[key]).length) createTree(liElem, data[key]);
+    }
+    element.appendChild(ulElem);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
